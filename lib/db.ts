f3f3d3ea83dcc24294ecbdae9db0373f0aada799
gauthi68-byte <gauthi68@gmail.com>
2026-08-23 11:10:07 +0200

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
