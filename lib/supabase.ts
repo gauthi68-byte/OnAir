1d2f544468755b3c090e28d1bffedfc17776aaf0
gauthi68-byte <gauthi68@gmail.com>
2026-08-23 15:19:49 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'sb_publishable_O8kpfYvsWHlAltKRqKAvsQ_T25UGM-w';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6c3RrcGpscGhlZnlkcG1sb2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0ODIzOTcsImV4cCI6MjEwMzA1ODM5N30.InJERHbFsSyb1BdHoPAquBgOG-gmiAt6sD00m0SF4bQ';
-https://github.com/gauthi68-byte/OnAir/blob/main/lib/supabase.ts
 export const supabase = createClient(supabaseUrl, supabaseKey);
